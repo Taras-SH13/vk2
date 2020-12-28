394a153825b5b487b7fb25ae3f61d333eeb694ff
Taras-SH13 <13tsh2019@gmail.com>
2020-12-29 00:38:18 +0200

    Add fields.

--- app/src/main/java/com/example/vkinfo/MainActivity.java
+++ app/src/main/java/com/example/vkinfo/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.vkinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
